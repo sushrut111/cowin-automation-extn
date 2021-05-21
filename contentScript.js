a2c6b9fb75164c375c9044996c93e5aa3dc9ce71
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-05-21 18:30:12 +0530

    Updated modal title

--- contentScript.js
+++ contentScript.js
@@ -530,7 +530,7 @@ const createModal = () => {
   <div class="modal-dialog modal-dialog-centered modal-xl">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Form</h5>
+        <h5 class="modal-title">Autofill Input Form</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
       </div>
       <div class="modal-body" id="form-modal-body">
