7e78b578c5b3068f9a1190dfbda391794fb17e03
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-05-22 20:15:21 +0530

    restructured alt+x

--- eventListeners.js
+++ eventListeners.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-    document.addEventListener("keydown", function (event) {
+    $(document).on("keydown", function (event) {
         if (event.altKey && (event.keyCode === 88)) {
             $("#form-modal").modal('toggle');
             event.preventDefault();
         }
     });
+
 
     $(".toggle-modal").on('click', () => {
         $("#form-modal").modal('toggle');
