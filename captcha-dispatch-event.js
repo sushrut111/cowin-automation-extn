ee5e2a532b19ca463fe00f0da2046cca42e4f934
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-05-29 14:00:27 +0530

    added waitForEl

--- captcha-dispatch-event.js
+++ captcha-dispatch-event.js
@@ -1,5 +1,4 @@
 $(document).on('click', 'ul.slot-available-wrap li a', async function (e) {
     await sleep(1000)
-    if ($('.appoint-success').length > 0)
-        enterCaptcha()
+    waitForEl('.time-slot', enterCaptcha)
 })
